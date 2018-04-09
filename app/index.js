393ec7b16ddb2be9f9c5aba4fc06612fc903e76a
Alexandre Rodriguez <alexandre@identiq.net>
2018-04-09 12:02:23 +0200

    init project

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import $ from "jquery";
+import 'bootstrap';
+import 'fullpage.js';
+import 'styles/index.scss';
+
+
+$(function() {
+  $(document).ready(function() {
+    $('#fullpage').fullpage();
+  });
+});

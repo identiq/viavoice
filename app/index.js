0c8092663d1cf05eaf3e8908386475880abf1eff
Alexandre Rodriguez <alexandre@identiq.net>
2018-04-09 14:45:34 +0200

    Add range and fullpage

--- app/index.js
+++ app/index.js
@@ -1,11 +1,33 @@
 import $ from "jquery";
+import noUiSlider from "nouislider";
 import 'bootstrap';
 import 'fullpage.js';
 import 'styles/index.scss';
 
 
 $(function() {
-  $(document).ready(function() {
-    $('#fullpage').fullpage();
+  $('#fullpage').fullpage({
+    anchors: ['firstPage', 'secondPage', '3rdPage'],
+    sectionsColor: ['#FAFAFA', '#fff', '#FAFAFA'],
+    navigation: true,
+    navigationPosition: 'right',
+    navigationTooltips: ['First', 'Second', 'Third']
+  });
+
+  let slider = document.getElementById('range');
+
+  noUiSlider.create(slider, {
+    start: [20, 80],
+    connect: true,
+    orientation: 'vertical',
+    range: {
+      'min': 0,
+      'max': 100
+    },
+    pips: {
+      mode: 'steps',
+      stepped: true,
+      density: 4
+    }
   });
 });

cb2269557f1c702f43df5d970dd77c2c135cec03
Alexandre Rodriguez <alexandre@identiq.net>
2018-06-18 17:32:57 +0200

    Add all charts

--- app/index.js
+++ app/index.js
@@ -9,6 +9,7 @@ import 'chartjs-plugin-annotation';
 import 'styles/index.scss';
 
 $(function () {
+
   $('#fullpage').fullpage({
     anchors: ['firstPage', 'secondPage', '3rdPage'],
     sectionsColor: ['#fff', '#fff', '#fff'],
@@ -43,14 +44,32 @@ $(function () {
     }
   });
 
+  const barColors = [
+    'rgba(148, 138, 84, 1)',
+    'rgba(148, 138, 84, 0.75)',
+    'rgba(148, 138, 84, 0.5)',
+    'rgba(0, 0, 0, 0.3)',
+    'rgba(0, 0, 0, 0.6)',
+    'rgba(0, 0, 0, 0.05)'
+  ];
+
+  const barLabels = [
+    `S’améliorera  nettement`,
+    `S’améliorera  un peu`,
+    `Restera  stationnaire`,
+    `Se dégradera  un peu`,
+    `Se dégradera  nettement`,
+    `Non réponse`
+  ];
+
+
   Chart.defaults.scale.gridLines.display = false;
   Chart.defaults.global.legend.display = false;
-  Chart.defaults.global.layout.padding = 30;
+  Chart.defaults.global.layout.padding.top = 30;
   Chart.defaults.global.defaultFontFamily = "'Avenir Next', sans-serif";
   Chart.defaults.global.defaultFontColor = "#000";
 
   let allCtx = document.getElementById('allChart').getContext('2d');
-  let periodCtx = document.getElementById('periodChart').getContext('2d');
 
   let gradientStroke = allCtx.createLinearGradient(150, 0, 150, 300);
   gradientStroke.addColorStop(.7, "#948A54");
@@ -64,11 +83,11 @@ $(function () {
     data: {
       labels: ["January", "February", "March", "April", "May", "June", "July"],
       datasets: [{
-        borderColor:               gradientStroke,
-        pointBorderColor:          gradientStroke,
-        pointBackgroundColor:      gradientStroke,
+        borderColor: gradientStroke,
+        pointBorderColor: gradientStroke,
+        pointBackgroundColor: gradientStroke,
         pointHoverBackgroundColor: gradientStroke,
-        pointHoverBorderColor:     gradientStroke,
+        pointHoverBorderColor: gradientStroke,
         data: [0, 10, 5, 2, 20, 30, 45],
         fill: false,
         datalabels: {
@@ -124,6 +143,8 @@ $(function () {
       }
     }
   });
+
+  let periodCtx = document.getElementById('periodChart').getContext('2d');
 
   let periodChart = new Chart(periodCtx, {
     // The type of chart we want to create
@@ -133,11 +154,11 @@ $(function () {
     data: {
       labels: ["January", "February", "March", "April", "May", "June", "July"],
       datasets: [{
-        borderColor:               gradientStroke,
-        pointBorderColor:          gradientStroke,
-        pointBackgroundColor:      gradientStroke,
+        borderColor: gradientStroke,
+        pointBorderColor: gradientStroke,
+        pointBackgroundColor: gradientStroke,
         pointHoverBackgroundColor: gradientStroke,
-        pointHoverBorderColor:     gradientStroke,
+        pointHoverBorderColor: gradientStroke,
         data: [0, 10, 5, 2, 20, 30, 45],
         fill: false,
         datalabels: {
@@ -193,4 +214,142 @@ $(function () {
       }
     }
   });
+
+  let  barCtx = document.getElementById('barChart').getContext('2d');
+
+  let barChart = new Chart(barCtx, {
+    type: 'bar',
+    data: {
+      labels: barLabels,
+      datasets: [{
+        data: [1, 10, 5, 2, 20, 30],
+        backgroundColor: barColors,
+        datalabels: {
+          align: 'top',
+          anchor: 'end',
+          color: barColors
+        }
+      }]
+    },
+    // Configuration options go here
+    options: {
+      scales: {
+        yAxes: [
+          {
+            display: false,
+            gridLines: {
+              drawBorder: false
+            }
+          }
+        ],
+        xAxes: [
+          {
+            ticks: {
+              fontColor: 'rgba(0, 0, 0, .5)',
+              autoSkip: false
+            }
+          }
+        ]
+      },
+      annotation: {
+        annotations: [
+          {
+            drawTime: "afterDatasetsDraw",
+            type: "line",
+            mode: "vertical",
+            scaleID: "x-axis-0",
+            value: "February",
+            borderColor: "rgba(0, 0, 0, .3)",
+            borderWidth: 1,
+            label: {
+              fontFamily: "'Avenir Next', sans-serif",
+              fontStyle: "400",
+              backgroundColor: "rgba(255, 255, 255, 1)",
+              yAdjust: -200,
+              yPadding: 30,
+              fontColor: "#000",
+              content: "Présidentielle 2017",
+              enabled: true
+            },
+          }
+        ]
+      }
+    }
+  });
+
+  let evolCtx = document.getElementById('evolChart').getContext('2d');
+
+  let evolChart = new Chart(evolCtx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      datasets: [
+        {
+          label: `Sous-total « S'améliorera » (en %)`,
+          data: [45, 30, 20, 2, 5, 10, 1],
+          fill: false,
+          borderColor: '#948A54',
+          datalabels: {
+            align: 'top',
+            anchor: 'end',
+            color: '#948A54'
+          }
+        },
+        {
+          label: `Sous-total « Se dégradera » (en %)`,
+          data: [1, 10, 5, 2, 20, 30, 45],
+          fill: false,
+          borderColor: '#9B9B9B',
+          datalabels: {
+            align: 'top',
+            anchor: 'end',
+            color: '#9B9B9B'
+          }
+        }
+      ]
+    },
+    // Configuration options go here
+    options: {
+      legend: {
+        display: true,
+        labels: {
+          boxWidth: 10,
+          fontSize: 10,
+          fontColor: '#4A4A4A'
+
+        }
+      },
+      title: {
+        fontColor: '#4A4A4A',
+        fontStyle: 400,
+        fontSize: 14,
+        display: true,
+        text: 'Évolution sur l’année'
+      },
+      scales: {
+        yAxes: [
+          {
+            display: false,
+            gridLines: {
+              drawBorder: false
+            }
+          }
+        ],
+        xAxes: [
+          {
+            ticks: {
+              fontColor: 'rgba(0, 0, 0, .5)',
+              autoSkip: false,
+              maxRotation: 80,
+              minRotation: 45
+            }
+          }
+        ]
+      }
+    }
+  });
+
 });
